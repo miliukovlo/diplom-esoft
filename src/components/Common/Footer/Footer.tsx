0d3aaf8b7bd49adb3d1de983436f24211e0a5e73
ssadown <bibatututu@yandex.ru>
2024-05-19 13:22:22 +0500

    Сделал футер

---
+++ src/components/Common/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FooterStyle.css'
+
+const Footer : React.FC = () => {
+    return (
+        <footer className='footer'>
+            <h1 className='footer-title'>Какой-нибудь текст</h1>
+        </footer>
+    );
+}
+
+export default Footer;
