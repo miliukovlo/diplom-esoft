d5aeaa466fe86c1af3e17ec7b768b770a3673563
ssadown <bibatututu@yandex.ru>
2024-06-12 13:10:42 +0500

    Вывел описание компании на ее странице в отдельный компонент

--- src/components/CompanyPage/CompanyContent.tsx
+++ src/components/CompanyPage/CompanyContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CompanyInterface from '../../Interfaces/CompanyInterface';
 import './CompanyContentStyle.css'
+import CompanyHeader from './CompanyHeader/CompanyHeader';
 
 interface CurrentCompanyInterface extends CompanyInterface {
     
@@ -15,22 +16,14 @@ const CompanyContent: React.FC<CurrentCompanyInterface> = ({
     id
 }: CurrentCompanyInterface) => {
     return (
-        <div className='company-content'>
-            <div className="company-content__logo-block">
-                <img src={logo} alt="Логотип компании" className='company-content__logo' />
-            </div>
-            <div className="company-content__information-block">
-                <div className="information-block__title">
-                    <h2 className='title-name'>{name}</h2>
-                    <p className='common-text'>{slogan}</p>
-                </div>
-                <div className="information-block__about">
-                    <p className="common-text">Специализация компании: {specialization === 'apartment' ?
-                'Квартиры' : 'Частные дома'
-            }</p>
-            <p className="common-text">Рейтинг: {rating}</p>
-                </div>
-            </div>
+        <div className="company-content">
+            <CompanyHeader
+                logo={logo}
+                name={name}
+                slogan={slogan}
+                rating={rating}
+                specialization={specialization}
+            />
         </div>
     );
 }
---
+++ src/components/CompanyPage/CompanyHeader/CompanyHeader.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './CompanyHeaderStyle.css'
+
+interface CompanyHeaderProps {
+    logo: string,
+    name: string,
+    slogan: string,
+    specialization: string,
+    rating: number
+}
+
+const CompanyHeader: React.FC<CompanyHeaderProps> = ({
+    logo,
+    name,
+    slogan,
+    rating,
+    specialization,
+}: CompanyHeaderProps) => {
+    return (
+        <>
+            <div className="company-content__logo-block">
+                <img src={logo} alt="Логотип компании" className='company-content__logo' />
+            </div>
+            <div className="company-content__information-block">
+                <div className="information-block__title">
+                    <h2 className='title-name'>{name}</h2>
+                    <p className='common-text'>{slogan}</p>
+                </div>
+                <div className="information-block__about">
+                    <p className="common-text">Специализация компании: {specialization === 'apartment' ?
+                'Квартиры' : 'Частные дома'
+            }</p>
+            <p className="common-text">Рейтинг: {rating}</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CompanyHeader;
